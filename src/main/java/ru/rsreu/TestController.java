e45ab9fb546ba5ee326eedc6452fa437b6fa2190
Alexey_Perov <perov-alexey@yandex.ru>
2016-06-28 20:04:17 +0300

    pom.xml changing

--- src/main/java/ru/rsreu/TestController.java
+++ src/main/java/ru/rsreu/TestController.java
@@ -2,16 +2,23 @@ package ru.rsreu;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class TestController {
 
-    @RequestMapping("test")
-    public ModelAndView testMethod() {
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("testView");
-        return mav;
+    @RequestMapping("/test")
+    public @ResponseBody Object testMethod() {
+//        ModelAndView mav = new ModelAndView();
+//        mav.setViewName("testView");
+//        return mav;
+        List test = new ArrayList();
+        test.add("sdfsdf");
+        return test;
     }
 
 }
