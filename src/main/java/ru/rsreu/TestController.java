a1f6b20111ffbe856c9638003153a434ffda7494
Alexey_Perov <perov-alexey@yandex.ru>
2016-06-29 01:26:47 +0300

    Change servlets version to 2.5, wrote test controller

--- src/main/java/ru/rsreu/TestController.java
+++ src/main/java/ru/rsreu/TestController.java
@@ -1,9 +1,12 @@
 package ru.rsreu;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import ru.rsreu.tracer.Shop;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,14 +14,21 @@ import java.util.List;
 @Controller
 public class TestController {
 
-    @RequestMapping("/test")
-    public @ResponseBody Object testMethod() {
-//        ModelAndView mav = new ModelAndView();
-//        mav.setViewName("testView");
-//        return mav;
-        List test = new ArrayList();
-        test.add("sdfsdf");
-        return test;
+    @RequestMapping(value="/test", method = RequestMethod.GET)
+    public @ResponseBody Shop getShopInJSON(@PathVariable String name) {
+
+        Shop shop = new Shop();
+        shop.setName(name);
+        shop.setStaffName(new String[]{"mkyong1", "mkyong2"});
+
+        return shop;
+
+    }
+
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public @ResponseBody String returnString(@PathVariable String name) {
+        return "hello!";
+
     }
 
 }
