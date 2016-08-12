650f35c2eda67edf7ee1339cd6a28dc73ac74d21
perov-alexey <perov-alexey@yandex.ru>
2016-08-12 23:55:08 +0300

    Add send function to the angular tracer controller

--- src/main/java/ru/rsreu/controllers/TracerController.java
+++ src/main/java/ru/rsreu/controllers/TracerController.java
@@ -2,6 +2,7 @@ package ru.rsreu.controllers;
 
 import org.springframework.web.bind.annotation.*;
 import ru.rsreu.tracer.algorithms.Algorithm;
+import ru.rsreu.tracer.algorithms.FirstAlgorithm;
 import ru.rsreu.tracer.algorithms.SecondAlgorithm;
 import ru.rsreu.tracer.helpers.FieldHelper;
 import ru.rsreu.tracer.pojo.Field;
@@ -11,7 +12,7 @@ public class TracerController {
 
     @RequestMapping(value = "/field", method = RequestMethod.POST, headers = {"content-type=application/json"})
     private @ResponseBody Field traceField(@RequestBody Field field) {
-        Algorithm alg = new SecondAlgorithm();
+        Algorithm alg = new FirstAlgorithm();
         FieldHelper.prepareLinks(field);
         alg.execute(field);
         return field;
--- src/main/webapp/resources/controllers/lightweightGenerator.controller.js
+++ src/main/webapp/resources/controllers/lightweightGenerator.controller.js
@@ -7,7 +7,21 @@ angular.module('tracer')
             $scope.field = new LightweightGenerator().generateField();
 
             $scope.traceField = function() {
-                alert($scope.field);
+                $.ajax({
+                    "url": "field",
+                    "data": JSON.stringify($scope.field),
+                    "method": "POST",
+                    'dataType': 'application/json',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    }
+                }).fail(function() {
+                    alert("fail!");
+                }).
+                done(function(data) {
+                    $scope.field = JSON.parse(data.responseText);
+                })
             }
 
         }
