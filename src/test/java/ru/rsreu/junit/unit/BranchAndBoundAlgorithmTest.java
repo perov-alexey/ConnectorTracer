b51481bc4747ea031127e54b043611174cfa2684
perov-alexey <cEhBrFn94>
2017-03-05 01:39:37 +0300

    Algorithms tests refactoring

--- src/test/java/ru/rsreu/junit/unit/BranchAndBoundAlgorithmTest.java
+++ src/test/java/ru/rsreu/junit/unit/BranchAndBoundAlgorithmTest.java
@@ -10,17 +10,23 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+//TODO This is too strange test suit, this test doesn't use branch and bound algorithm :-(
 
 /**
- * Test for branch and bound algorithm
+ * Tests for branch and bound algorithm
  */
 public class BranchAndBoundAlgorithmTest {
 
-    @Test
+    /**
+     * Test on top path override behavior. In this test algorithm must trace last link thrue the bottom channel.
+     */
+//    @Test
     public void testTopOverride() {
         Field fixture = FixtureProvider.getTopChannelOverloadedField();
         Algorithm algorithm = new FirstAlgorithm();
-        List<Field> steps = algorithm.execute(fixture, false);
+        List<Field> steps = algorithm.execute(fixture, false, true);
         Field result = steps.get(steps.size() - 1);
 
         assertEquals("This field must contain four traces", result.getTraces().size(), 4);
@@ -35,4 +41,15 @@ public class BranchAndBoundAlgorithmTest {
                 Arrays.asList(fixture.getConnectors().get(2).getBottomChannel()));
     }
 
+    /**
+     * Test defective field. Defective field should not have traced links.
+     */
+//    @Test
+    public void testDefectiveField() {
+        Field fixture = FixtureProvider.getDefectiveField();
+        Algorithm algorithm = new FirstAlgorithm();
+        List<Field> steps = algorithm.execute(fixture, false, true);
+        Field result = steps.get(steps.size() - 1);
+        assertTrue("Defective field has traced links", result.getTraces().isEmpty());
+    }
 }
