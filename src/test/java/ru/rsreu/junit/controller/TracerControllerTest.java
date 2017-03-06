840ac07037c5dc823b4c4f263e9f528d2f863f33
perov-alexey <cEhBrFn94>
2017-03-06 22:08:34 +0300

    Fix field controller test

--- src/test/java/ru/rsreu/junit/controller/TracerControllerTest.java
+++ src/test/java/ru/rsreu/junit/controller/TracerControllerTest.java
@@ -15,7 +15,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import ru.rsreu.tracer.pojo.Field;
+import ru.rsreu.junit.utils.FixtureProvider;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -34,7 +34,7 @@ public class TracerControllerTest {
 
     @Test
     public void testAccessTracerController() throws Exception {
-        String fieldJson = new ObjectMapper().writeValueAsString(new Field());
+        String fieldJson = new ObjectMapper().writeValueAsString(FixtureProvider.getTopChannelOverloadedField());
         this.mockMvc.perform(post("/field")
                 .content(fieldJson)
                 .contentType(MediaType.APPLICATION_JSON))
